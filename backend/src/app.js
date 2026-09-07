5774857e234b7fcc886f3a75ea18db0edb76925d
kaveeshaobada <obadakumburakaveesha@gmail.com>
2026-09-07 10:56:08 +0530

    chore: add production build scripts and enable proxy trust configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,6 +13,7 @@ const userRoutes = require("./routes/users");
 const contactRoutes = require("./routes/contacts");
 
 const app = express();
+app.set("trust proxy", 1);
 
 app.use(helmet());
 app.use(
